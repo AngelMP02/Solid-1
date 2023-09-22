3e90b0a9047b75238aa0ba6c52bb213f06d3facd
paulaanb <91721496+paulaanb@users.noreply.github.com>
2023-09-22 15:18:10 +0000

    hola

---
+++ ej1.py
@@ -0,0 +1,1 @@
+Para este ejercicio, deberás crear una clase que representa una matriz. Las operaciones que esta clase debe permitir son la creación de una matriz a partir de una lista de listas, la impresión de la matriz en una forma legible, y el cálculo de la transpuesta de la matriz. Asegúrate de que cada método tenga una única responsabilidad.
