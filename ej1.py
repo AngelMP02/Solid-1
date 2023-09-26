e1c7d5369f6855271a88424bd514492221360ba7
AngelMP02 <94680353+AngelMP02@users.noreply.github.com>
2023-09-26 14:44:51 +0200

    Update ej1.py

--- ej1.py
+++ ej1.py
@@ -13,3 +13,21 @@ class Matrix:
             for j in range(len(self.matrix[0])):
                 print(self.matrix[j][i], end=' ')
             print()
+         
+# Definir una matriz de ejemplo
+mi_matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# Crear un objeto de la clase Matrix
+matriz_objeto = Matrix(mi_matriz)
+
+# Imprimir la matriz original
+print("Matriz original:")
+matriz_objeto.print()
+
+# Imprimir la matriz transpuesta
+print("\nMatriz transpuesta:")
+matriz_objeto.transpose()
